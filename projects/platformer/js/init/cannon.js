c231ae1359669702add8e85730d558dc9ddd3cbd
Hunt3ra198 <h.alfrey08@gmail.com>
2022-11-03 15:01:03 +0000

    fixed error in platformer

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -28,7 +28,7 @@
 
     // example:
     createCannon("top", 450);
-    createCannon("f", 450); // a cannon on the top of the screen, located at x = 450
+    createCannon("left", 450); // a cannon on the top of the screen, located at x = 450
     createCannon("left", 300, 1000); // a cannon on the left side of the screen, located at y = 300, with a 1 second delay (1000ms)
 
 
