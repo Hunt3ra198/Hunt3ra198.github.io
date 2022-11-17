92a73ded05147c542a85e40e15b1d2977642964c
Hunt3ra198 <h.alfrey08@gmail.com>
2022-11-17 15:30:04 +0000

    drew 5 cirlces and movement stuck on todo 6

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,17 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-  var circle;			// variable to hold a single circle when creating circles / iterating
-  var circles = [];	// variable to store all circles in one Array
-
-
+        var circle;
+        var circles = [];
+    
         // TODO 2 : Create a function that draws a circle 
-        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-        physikz.addRandomVelocity(circle, canvas);
-        view.addChild(circle);
-        circles.push(circle);
+        var drawCircle = function() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            circles.push(circle);
+            view.addChild(circle);
+        }
+    
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
         drawCircle();
 
 
@@ -46,6 +52,11 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            physikz.updatePosition(circles[0])
+            physikz.updatePosition(circles[1])
+            physikz.updatePosition(circles[2])
+            physikz.updatePosition(circles[3])
+            physikz.updatePosition(circles[4])
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
